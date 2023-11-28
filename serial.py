e2c549266c3293e0f9f0e8ab9a713f19bcda97f0
Nathan Rydel <nathanrydel@gmail.com>
2023-11-28 16:18:11 -0700

    create initializer func and added general todos

--- serial.py
+++ serial.py
@@ -1,6 +1,6 @@
 class SerialGenerator:
     """Machine to create unique incrementing serial numbers.
-    
+
     >>> serial = SerialGenerator(start=100)
 
     >>> serial.generate()
@@ -17,3 +17,12 @@ class SerialGenerator:
     >>> serial.generate()
     100
     """
+
+    def __init__(self, start=0):
+        """constructor function to create a new SerialGenerator instance"""
+
+        self.start = start
+
+    # TODO: make a .generate method
+
+    # TODO: make a .reset method
