94f4297085ac26a07138e6011eb1005b236fad88
Veronica Ni <vni1294@gmail.com>
2023-11-28 19:18:19 -0500

    added __init__, implemented getWords

--- wordfinder.py
+++ wordfinder.py
@@ -1,2 +1,17 @@
 class WordFinder:
     """Word Finder: finds random words from a dictionary."""
+
+    def __init__(self, path):
+        """Makes a list of the words contained in the file of the path"""
+        file = open(path)
+
+        # make a self.words = [word, word, word]
+        self.words = self.getWords(file)
+
+    # call a print method
+    def getWords(self, file):
+        return [word.strip() for word in file]
+
+    def printWords(self):
+        # print 'len(self.words) words read'
+        ...
